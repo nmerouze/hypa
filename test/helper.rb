d09f6cba42b4c327367e58c02b7536496eeb5aca
Nicolas Mérouze <nicolas.merouze@gmail.com>
2013-05-25 10:55:27 +0200

    Reactivate coverage

--- test/helper.rb
+++ test/helper.rb
@@ -2,27 +2,28 @@
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'rack/test'
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  project_name 'hypa'
+  minimum_coverage 80
+
+  add_filter '/spec/'
+  add_filter '/vendor/'
+  add_group 'Library', 'lib'
+end
+
 require 'hypa'
 require 'sinatra'
 require 'active_record'
 require 'active_model/serializer'
-
-# require 'simplecov'
-# require 'coveralls'
-
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-#   SimpleCov::Formatter::HTMLFormatter,
-#   Coveralls::SimpleCov::Formatter
-# ]
-
-# SimpleCov.start do
-#   project_name 'hypa'
-#   minimum_coverage 80
-
-#   add_filter '/spec/'
-#   add_filter '/vendor/'
-#   add_group 'Library', 'lib'
-# end
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database  => ':memory:')
 
